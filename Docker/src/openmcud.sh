789c316a12d0349a18ad375a11ab0fae42a185a0
johannrichard <johann.richard@mycvs.org>
2017-06-23 23:10:18 +0200

    Updatred Dockerfile with more options and volumes

--- Docker/src/openmcud.sh
+++ Docker/src/openmcud.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-exec /usr/bin/openmcu-ru -x >>/var/log/openmcu.log 2>&1
+# Run as user mcu (per installation)
+exec /sbin/setuser mcu /usr/bin/openmcu-ru -x -i /etc/openmcu-ru/settings.ini >>/var/log/openmcu.log 2>&1
