a2de097b8b3fc08b7bb9f529fe982960063472f2
johannrichard <johann.richard@mycvs.org>
2017-06-23 23:53:21 +0200

    More finetuning and better config handling

--- Docker/src/openmcud.sh
+++ Docker/src/openmcud.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Run as user mcu (per installation)
-exec /sbin/setuser mcu /usr/bin/openmcu-ru -x -i /etc/openmcu-ru/settings.ini >>/var/log/openmcu.log 2>&1
+exec /sbin/setuser mcu /usr/bin/openmcu-ru -x -i /opt/openmcu-ru/config/openmcu.ini >>/var/log/openmcu.log 2>&1
